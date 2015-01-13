a025008d684de86262f7173b714dc1df9ea32218
wzdark <wzfindjob@163.com>
2015-01-13 16:28:38 +0800

    新增java

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		String  s1 = "a";
+		String s2 = "ab";
+		String s3 = s1 + "b";
+		System.out.println(s3 == s2);
+
+	}
+}
+
